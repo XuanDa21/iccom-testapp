7f4d45d2299e22e228638ee6fadc3e2d2be6d252
XuanDa21 <datrinh318@gmail.com>
2024-10-31 12:54:35 +0700

    Update config.py

--- automation_test/common/config.py
+++ automation_test/common/config.py
@@ -5,19 +5,14 @@ SERIAL_PORT="/dev/ttyUSB0"
 board_NO = "7030"
 PLATFORM = ""
 if SOC == "E3v1.1":
-    NUM_CPU=2
     PLATFORM="ebisu"
 elif SOC.find("H3v3.0") != -1:
-    NUM_CPU=8
     PLATFORM="salvator-x"
 elif SOC.find("M3Nv1.1") != -1:
-    NUM_CPU=2
     PLATFORM="salvator-x"    
 elif SOC.find("M3v3.0") != -1:
-    NUM_CPU=6
     PLATFORM="salvator-x"
 elif SOC.find("M3v1.2") != -1:
-    NUM_CPU=6
     PLATFORM="salvator-x"
 
 FEATBOX=True
