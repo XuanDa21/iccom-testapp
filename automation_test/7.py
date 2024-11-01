a922b02475f79a0473b9f0aad6fa796fcbd1cbff
XuanDa21 <datrinh318@gmail.com>
2024-11-01 15:56:23 +0700

    Fix 7.py

--- automation_test/7.py
+++ automation_test/7.py
@@ -12,10 +12,8 @@ if __name__ == '__main__':
     a=SerialThread(config.SERIAL_PORT,1, config.MODULE_TEST)
     a.start()
     print_result.print_item(a,7,"Normal")
-    a.send("modprobe iccom")
     for i in range(100):
         a.send("modprobe iccom")
         sleep(0.1)
         a.send("rmmod iccom.ko")
-    a.send("rmmod iccom.ko")
     print_result.func_pass(a)
